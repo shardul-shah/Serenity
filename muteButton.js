5fd6be78c11d7208b6bbc550c6f73d49d147f442
Shardul <shardul@ualberta.ca>
2019-06-06 15:11:26 -0600

    Using local images instead of image links, in case the image hosting site goes down

--- muteButton.js
+++ muteButton.js
@@ -4,8 +4,8 @@ var volumeSlider = document.getElementById('custom_range')
 function toggleMute() {
 	// if sound is currently playing (and thus mute icon is being displayed), and the mute icon is clicked
 	// mute the sound, change the icon cuurrently displayed to speaker, adjust the volume slider accordingly
-	if (mute_icon.src == "http://upload.wikimedia.org/wikipedia/commons/3/3f/Mute_Icon.svg") {
-		mute_icon.src = "http://upload.wikimedia.org/wikipedia/commons/2/21/Speaker_Icon.svg";
+	if (mute_icon.src == "mute_icon.svg") {
+		mute_icon.src = "speaker_icon.svg";
 		sound.volume = 0;
 		custom_range.value = 0
 	}
@@ -14,7 +14,7 @@ function toggleMute() {
 	// unmute the sound to the volume it was before (stored in var volume), and adjust the slider back to where it was before
 	// and change the icon currently displayed to mute 
 	else {
-		mute_icon.src = "http://upload.wikimedia.org/wikipedia/commons/3/3f/Mute_Icon.svg";	
+		mute_icon.src = "mute_icon.svg";	
 		sound.volume = volume;
 		custom_range.value = volume*100
 	}
--- slider.js
+++ slider.js
@@ -6,8 +6,8 @@ function adjustVolume(newSliderValue) {
 	// we must keep a copy of the old sound volume value, for the case of the user changing the slider value but then muting sound	
 	volume = sound.volume;
 
-	if (mute_icon.src == "http://upload.wikimedia.org/wikipedia/commons/2/21/Speaker_Icon.svg")
-		mute_icon.src = "http://upload.wikimedia.org/wikipedia/commons/3/3f/Mute_Icon.svg";	
+	if (mute_icon.src == "speaker_icon.svg")
+		mute_icon.src = "mute_icon.svg";	
 
 		// leave below line for testing purposes 
 		// console.log(sound.volume, newSliderValue)
