eff87bc764fce5e9d7e228b72675fb1ec7a98b8a
Shardul <shardul@ualberta.ca>
2019-07-17 19:34:40 -0600

    Fixing small mistake

--- collapsibleButton.js
+++ collapsibleButton.js
@@ -1,9 +1,6 @@
 var collapsible_btn = document.getElementById("collapsible");
 var collapsed_div = document.getElementById("collapsed_div"); 
 
-collapsible_btn.addEventListener("hover", displayGlowEffect);
-
-
 function toggleCollapsible() {
 	if (window.getComputedStyle(collapsed_div).display == "none") {
 
