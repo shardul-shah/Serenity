c2ff005afcd4acaa076ecae6fd7fae20284003fc
Shardul <shardul@ualberta.ca>
2019-11-30 19:21:43 -0700

    More bug fixing, fixing comments, changing image based mute icons to fully text based (FontAwesome) ones, reducing image clutter, improving CSS

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,23 +1,27 @@
 var body = document.body;
 var text = document.getElementsByClassName("text");
 var element_icons = document.getElementsByClassName("slider_icons");
+var mute_icons = document.getElementsByClassName("mute_icon");
 
 function toggleDarkMode() {
 	to_top.classList.toggle("to_top");
 	for (i=0; i<sliders.length; i++) 
-		console.log(sliders[i].classList.toggle("slider"));
+		sliders[i].classList.toggle("slider");
 
 	if (body.classList.toggle("dark_mode_bg") == true) {
 		for (i=0; i<text.length; i++) 
 			text[i].style.color = "rgba(255, 255, 255, 1)";
 		for (i=0; i<element_icons.length; i++) 
-			element_icons[i].style.color = "rgba(255, 255, 255, 1)";	
-
+			element_icons[i].style.color = "rgba(255, 255, 255, 1)";
+		for (i=0; i<mute_icons.length; i++)
+			mute_icons[i].style.color = "rgba(255, 255, 255, 1)";
 	}
 	else {
 		for (i=0; i<text.length; i++) 
 			text[i].style.color = "rgba(0, 0, 0, 1)";
 		for (i=0; i<element_icons.length; i++) 
 			element_icons[i].style.color = "rgba(0, 0, 0, 1)";
+		for (i=0; i<mute_icons.length; i++)
+			mute_icons[i].style.color = "rgba(0, 0, 0, 1)";
 	}
-}
+};
--- scripts/muteButton.js
+++ scripts/muteButton.js
@@ -34,102 +34,81 @@ function toggleMute(soundType) {
 	// if sound is currently playing (and thus mute icon is being displayed), and the mute icon is clicked
 	// mute the sound, change the icon cuurrently displayed to speaker, adjust the volume slider accordingly
 
-	/* IMPORTANT: the following is a small bug which made me stuck for a long time. A note for myself and others who read this.
-	If the intent is to to use relative filepaths, as one should, and a comparision to a src of a element (such as an image)
-	is needed, using element.src yields the ABSOLUTE filepaths, not the relative filepaths. Since often, comparisions are made with
-	relative filepaths, this is a common source of bugs. The solution is to use element.getAttribute('src'), which yields the proper
-	relative filepath.
-
-	EXAMPLE ON MY MACHINE:
-
-	If an <img> exists with id='mute_icon'...
-
-	DO NOT DO:
-
-	mute_icon.src == 'images/mute_icon.png'
-	as mute_icon.src yields:
-	file:///Users/Shardul/Desktop/Rain%20Project/images/mute_icon.png (absolute), not 'images/mute_icon.png' (relative).
-	
-	DO:
-
-	mute_icon.getAttribute('src') == 'images/mute_icon.png'
-	as mute_icon.getAttribute('src') yields:
-	'images/mute_icon.png' (relative).
-
-	Credit: https://stackoverflow.com/questions/18149043/how-to-check-img-src-is-true-using-javascript */
-
 	if (soundType == "rain") {
 
 		// if rain sound is currently playing and the speaker icon is clicked... 
-		if (rain_mute_icon.getAttribute('src') == "images/speaker_icon.png") {
-			rain_mute_icon.src = "images/mute_icon.png";
+		if (rain_mute_icon.className == "fas fa-volume-up mute_icon") {
+			rain_mute_icon.className = "fas fa-volume-mute mute_icon";
 			rain_sound.volume = 0;
 			rain_slider.value = 0;
-		}
+		}	
 
 		//case of slider manually being put to 0 and then sound being unmuted
-		else if (rain_mute_icon.getAttribute('src') == "images/mute_icon.png" && rain_volume == 0) {
-			rain_mute_icon.src = "images/speaker_icon.png";
+		else if (rain_mute_icon.className == "fas fa-volume-mute mute_icon" && rain_volume == 0) {
+			rain_mute_icon.className = "fas fa-volume-up mute_icon";
 			rain_sound.volume = 0.01;
 			rain_slider.value = 1;
 			rain_volume = rain_sound.volume;
 		}
 
-	// if rain sound is currently muted (and thus speaker icon is being displayed), and the speaker icon is clicked...
-	// unmute the sound to the volume it was before (stored in var rain_volume), and adjust the slider back to where it was before
-	// and change the icon currently displayed to mute 
+		// if rain sound is currently muted (and thus speaker icon is being displayed), and the speaker icon is clicked...
+		// unmute the sound to the volume it was before (stored in var rain_volume), and adjust the slider back to where it was before
+		// and change the icon currently displayed to mute 
 		else {
-			rain_mute_icon.src = "images/speaker_icon.png";	
+			rain_mute_icon.className = "fas fa-volume-up mute_icon";
 			rain_sound.volume = rain_volume;
 			rain_slider.value = rain_volume*100;
 		}
+	
 	}
 
 	else if (soundType == "ocean") {
 		// if ocean sound is currently playing and the speaker icon is clicked... 
-		if (ocean_mute_icon.getAttribute('src') == "images/speaker_icon.png") {
-			ocean_mute_icon.src = "images/mute_icon.png";
+		if (ocean_mute_icon.className == "fas fa-volume-up mute_icon") {
+			ocean_mute_icon.className = "fas fa-volume-mute mute_icon";
 			ocean_sound.volume = 0;
 			ocean_slider.value = 0;
-		}
+		}	
 
 		//case of slider manually being put to 0 and then sound being unmuted
-		else if (ocean_mute_icon.getAttribute('src') == "images/mute_icon.png" && ocean_volume == 0) {
-			ocean_mute_icon.src = "images/speaker_icon.png";
+		else if (ocean_mute_icon.className == "fas fa-volume-mute mute_icon" && ocean_volume == 0) {
+			ocean_mute_icon.className = "fas fa-volume-up mute_icon";
 			ocean_sound.volume = 0.01;
 			ocean_slider.value = 1;
 			ocean_volume = ocean_sound.volume;
 		}
 
+		// if ocean sound is currently muted (and thus speaker icon is being displayed), and the speaker icon is clicked...
+		// unmute the sound to the volume it was before (stored in var ocean_volume), and adjust the slider back to where it was before
+		// and change the icon currently displayed to mute 
 		else {
-			ocean_mute_icon.src = "images/speaker_icon.png";	
+			ocean_mute_icon.className = "fas fa-volume-up mute_icon";
 			ocean_sound.volume = ocean_volume;
 			ocean_slider.value = ocean_volume*100;
 		}
 	}
 
 	else if (soundType == 'forest') {
-
 		// if forest sound is currently playing and the speaker icon is clicked... 
-		if (forest_mute_icon.getAttribute('src') == "images/speaker_icon.png") {
-			forest_mute_icon.src = "images/mute_icon.png";
+		if (forest_mute_icon.className == "fas fa-volume-up mute_icon") {
+			forest_mute_icon.className = "fas fa-volume-mute mute_icon";
 			forest_sound.volume = 0;
 			forest_slider.value = 0;
-		}
+		}	
 
 		//case of slider manually being put to 0 and then sound being unmuted
-		else if (forest_mute_icon.getAttribute('src') == "images/mute_icon.png" && forest_volume == 0) {
-			forest_mute_icon.src = "images/speaker_icon.png";
+		else if (forest_mute_icon.className == "fas fa-volume-mute mute_icon" && forest_volume == 0) {
+			forest_mute_icon.className = "fas fa-volume-up mute_icon";
 			forest_sound.volume = 0.01;
 			forest_slider.value = 1;
 			forest_volume = forest_sound.volume;
 		}
 
-	// if forest sound is currently muted (and thus speaker icon is being displayed), and the speaker icon is clicked...
-	// unmute the sound to the volume it was before (stored in var volume), and adjust the slider back to where it was before
-	// and change the icon currently displayed to mute 
+		// if forest sound is currently muted (and thus speaker icon is being displayed), and the speaker icon is clicked...
+		// unmute the sound to the volume it was before (stored in var forest_volume), and adjust the slider back to where it was before
+		// and change the icon currently displayed to mute 
 		else {
-			forest_mute_icon.src = "images/speaker_icon.png";	
+			forest_mute_icon.className = "fas fa-volume-up mute_icon";
 			forest_sound.volume = forest_volume;
 			forest_slider.value = forest_volume*100;
 		}
@@ -138,25 +117,25 @@ function toggleMute(soundType) {
 	else if (soundType == 'wind') {
 
 		// if wind sound is currently playing and the speaker icon is clicked... 
-		if (wind_mute_icon.getAttribute('src') == "images/speaker_icon.png") {
-			wind_mute_icon.src = "images/mute_icon.png";
+		if (wind_mute_icon.className == "fas fa-volume-up mute_icon") {
+			wind_mute_icon.className = "fas fa-volume-mute mute_icon";
 			wind_sound.volume = 0;
 			wind_slider.value = 0;
-		}
+		}	
 
 		//case of slider manually being put to 0 and then sound being unmuted
-		else if (wind_mute_icon.getAttribute('src') == "images/mute_icon.png" && wind_volume == 0) {
-			wind_mute_icon.src = "images/speaker_icon.png";
+		else if (wind_mute_icon.className == "fas fa-volume-mute mute_icon" && wind_volume == 0) {
+			wind_mute_icon.className = "fas fa-volume-up mute_icon";
 			wind_sound.volume = 0.01;
 			wind_slider.value = 1;
 			wind_volume = wind_sound.volume;
 		}
 
-	// if wind sound is currently muted (and thus speaker icon is being displayed), and the speaker icon is clicked...
-	// unmute the sound to the volume it was before (stored in var volume), and adjust the slider back to where it was before
-	// and change the icon currently displayed to mute 
+		// if wind sound is currently muted (and thus speaker icon is being displayed), and the speaker icon is clicked...
+		// unmute the sound to the volume it was before (stored in var wind_volume), and adjust the slider back to where it was before
+		// and change the icon currently displayed to mute 
 		else {
-			wind_mute_icon.src = "images/speaker_icon.png";	
+			wind_mute_icon.className = "fas fa-volume-up mute_icon";
 			wind_sound.volume = wind_volume;
 			wind_slider.value = wind_volume*100;
 		}
@@ -165,24 +144,25 @@ function toggleMute(soundType) {
 	else if (soundType == 'fire') {
 
 		// if fire sound is currently playing and the speaker icon is clicked... 
-		if (fire_mute_icon.getAttribute('src') == "images/speaker_icon.png") {
-			fire_mute_icon.src = "images/mute_icon.png";
+		if (fire_mute_icon.className == "fas fa-volume-up mute_icon") {
+			fire_mute_icon.className = "fas fa-volume-mute mute_icon";
 			fire_sound.volume = 0;
 			fire_slider.value = 0;
-		}
+		}	
 
 		//case of slider manually being put to 0 and then sound being unmuted
-		else if (fire_mute_icon.getAttribute('src') == "images/mute_icon.png" && fire_volume == 0) {
-			fire_mute_icon.src = "images/speaker_icon.png";
+		else if (fire_mute_icon.className == "fas fa-volume-mute mute_icon" && fire_volume == 0) {
+			fire_mute_icon.className = "fas fa-volume-up mute_icon";
 			fire_sound.volume = 0.01;
 			fire_slider.value = 1;
 			fire_volume = fire_sound.volume;
 		}
-	// if fire sound is currently muted (and thus speaker icon is being displayed), and the speaker icon is clicked...
-	// unmute the sound to the volume it was before (stored in var volume), and adjust the slider back to where it was before
-	// and change the icon currently displayed to mute 
+
+		// if fire sound is currently muted (and thus speaker icon is being displayed), and the speaker icon is clicked...
+		// unmute the sound to the volume it was before (stored in var fire_volume), and adjust the slider back to where it was before
+		// and change the icon currently displayed to mute 
 		else {
-			fire_mute_icon.src = "images/speaker_icon.png";	
+			fire_mute_icon.className = "fas fa-volume-up mute_icon";
 			fire_sound.volume = fire_volume;
 			fire_slider.value = fire_volume*100;
 		}
@@ -190,27 +170,57 @@ function toggleMute(soundType) {
 
 	else {
 		// if thunder sound is currently playing and the speaker icon is clicked... 
-		if (thunder_mute_icon.getAttribute('src') == "images/speaker_icon.png") {
-			thunder_mute_icon.src = "images/mute_icon.png";
+		if (thunder_mute_icon.className == "fas fa-volume-up mute_icon") {
+			thunder_mute_icon.className = "fas fa-volume-mute mute_icon";
 			thunder_sound.volume = 0;
 			thunder_slider.value = 0;
-		}
+		}	
 
 		//case of slider manually being put to 0 and then sound being unmuted
-		else if (thunder_mute_icon.getAttribute('src') == "images/mute_icon.png" && thunder_volume == 0) {
-			thunder_mute_icon.src = "images/speaker_icon.png";
+		else if (thunder_mute_icon.className == "fas fa-volume-mute mute_icon" && thunder_volume == 0) {
+			thunder_mute_icon.className = "fas fa-volume-up mute_icon";
 			thunder_sound.volume = 0.01;
 			thunder_slider.value = 1;
 			thunder_volume = thunder_sound.volume;
 		}
 
-	// if thunder sound is currently muted (and thus speaker icon is being displayed), and the speaker icon is clicked...
-	// unmute the sound to the volume it was before (stored in var volume), and adjust the slider back to where it was before
-	// and change the icon currently displayed to mute 
+		// if thunder sound is currently muted (and thus speaker icon is being displayed), and the speaker icon is clicked...
+		// unmute the sound to the volume it was before (stored in var thunder_volume), and adjust the slider back to where it was before
+		// and change the icon currently displayed to mute 
 		else {
-			thunder_mute_icon.src = "images/speaker_icon.png";	
+			thunder_mute_icon.className = "fas fa-volume-up mute_icon";
 			thunder_sound.volume = thunder_volume;
 			thunder_slider.value = thunder_volume*100;
 		}
 	}
 };
+
+/* 
+IMPORTANT: the following is a small bug which made me stuck for a long time, when I initially had the mute and unmute button as images,
+and was replacing the images instead of the fontawesome icons that I have now. I want to leave it in case I deal with replacing images
+by its SRC again.
+
+ A note for myself and others who read this.
+If the intent is to to use relative filepaths, as one should, and a comparision to a src of a element (such as an image)
+is needed, using element.src yields the ABSOLUTE filepaths, not the relative filepaths. Since often, comparisions are made with
+relative filepaths, this is a common source of bugs. The solution is to use element.getAttribute('src'), which yields the proper
+relative filepath.
+
+EXAMPLE ON MY MACHINE:
+
+If an <img> exists with id='mute_icon'...
+
+DO NOT DO:
+
+mute_icon.src == 'images/mute_icon.png'
+as mute_icon.src yields:
+file:///Users/Shardul/Desktop/Rain%20Project/images/mute_icon.png (absolute), not 'images/mute_icon.png' (relative).
+
+DO:
+
+mute_icon.getAttribute('src') == 'images/mute_icon.png'
+as mute_icon.getAttribute('src') yields:
+'images/mute_icon.png' (relative).
+
+Credit: https://stackoverflow.com/questions/18149043/how-to-check-img-src-is-true-using-javascript 
+*/
